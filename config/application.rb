810ed35580c6cc48d03162651d9d7bcfc322f8d7
Olena Ageyeva <eageeva@hotmail.com>
2018-06-29 18:41:49 +0000

    automatically backed up by learn

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,13 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.before_configuration do
+  env_file = File.join(Rails.root, 'config', 'locales/en.yml')
+  YAML.load(File.open(env_file)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(env_file)
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
